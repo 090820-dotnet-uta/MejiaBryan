556f888a7cc0dd67844c2e2b428f47831c7f048b
bamejia <bamejia@udel.edu>
2020-09-09 16:17:16 -0400

    adds finished computer challenge 4

--- CodingChallenges/4_Methods/4_MethodsChallenge-StudentCopy/Program.cs
+++ CodingChallenges/4_Methods/4_MethodsChallenge-StudentCopy/Program.cs
@@ -23,30 +23,67 @@ namespace _4_MethodsChallenge
 
         public static string GetName()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            Console.WriteLine("Enter your name");
+            return Console.ReadLine();
         }
 
         public static void GreetFriend(string name)
         {
             //Greeting should be: Hello, nameVar. You are my friend
             //Ex: Hello, Jim. You are my friend
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            Console.WriteLine("Hello, {0}. You are my friend", name);
         }
 
         public static double GetNumber()
         {
             //Should throw FormatException if the user did not input a number
-            throw new NotImplementedException();
+            Console.WriteLine("Enter a double value");
+            string input = Console.ReadLine();
+            double double_input;
+            bool isDouble = double.TryParse(input, out double_input);
+            if (isDouble) return double_input;
+            throw new FormatException();
         }
 
         public static int GetAction()
         {
-            throw new NotImplementedException();
+            int int_input;
+            while(true){
+                Console.WriteLine("Please select action [1:Add, 2:Subtract, 3:Multiply, 4:Divide]");
+                string input = Console.ReadLine();
+                bool isInt = int.TryParse(input, out int_input);
+                if (isInt && (int_input > 0 && int_input <= 4)) break;
+            }
+            return int_input;
         }
 
         public static double DoAction(double x, double y, int z)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            double result = 0.0;
+            switch (z){
+                //Addition
+                case 1:
+                    result = x + y;
+                    break;
+                //Subtraction
+                case 2:
+                    result = y - x;
+                    break;
+                //Multiplication
+                case 3:
+                    result = x * y;
+                    break;
+                //Division
+                case 4:
+                    result = y / x;
+                    break;
+                default:
+                    throw new FormatException();
+            }
+            return result;
         }
     }
 }
